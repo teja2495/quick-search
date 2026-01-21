9fa395205457226a06d8b976e8ff1671fd82ae78
Teja Karlapudi <teja2495@gmail.com>
2026-01-20 19:24:38 -0500

    undo exclude

--- app/src/main/java/com/tk/quicksearch/search/searchScreen/SearchScreen.kt
+++ app/src/main/java/com/tk/quicksearch/search/searchScreen/SearchScreen.kt
@@ -6,16 +6,33 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
@@ -36,6 +53,7 @@ import com.tk.quicksearch.search.core.DirectSearchStatus
 import com.tk.quicksearch.search.core.SearchUiState
 import com.tk.quicksearch.search.core.SearchViewModel
 import com.tk.quicksearch.search.data.StaticShortcut
+import com.tk.quicksearch.search.data.shortcutDisplayName
 import com.tk.quicksearch.search.deviceSettings.DeviceSetting
 import com.tk.quicksearch.search.models.AppInfo
 import com.tk.quicksearch.search.models.ContactInfo
@@ -43,7 +61,10 @@ import com.tk.quicksearch.search.models.ContactMethod
 import com.tk.quicksearch.search.models.DeviceFile
 import com.tk.quicksearch.search.searchScreen.dialogs.NicknameDialogState
 import com.tk.quicksearch.search.searchScreen.dialogs.SearchScreenDialogs
+import com.tk.quicksearch.search.utils.FileUtils
+import com.tk.quicksearch.ui.theme.DesignTokens
 import com.tk.quicksearch.util.WallpaperUtils
+import kotlinx.coroutines.launch
 
 @Composable
 fun SearchRoute(
@@ -72,6 +93,91 @@ fun SearchRoute(
             remember(nicknameUpdateVersion) { { uri -> viewModel.getFileNickname(uri) } }
     val getSettingNickname: (String) -> String? =
             remember(nicknameUpdateVersion) { { id -> viewModel.getSettingNickname(id) } }
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val snackbarScope = rememberCoroutineScope()
+    val undoLabel = stringResource(R.string.action_undo)
+
+    val showUndoSnackbar: (String, () -> Unit) -> Unit = { message, onUndo ->
+        snackbarScope.launch {
+            val result =
+                    snackbarHostState.showSnackbar(
+                            message = message,
+                            actionLabel = undoLabel,
+                            duration = SnackbarDuration.Short
+                    )
+            if (result == SnackbarResult.ActionPerformed) {
+                onUndo()
+            }
+        }
+    }
+
+    val onHideAppWithUndo: (AppInfo) -> Unit = { app ->
+        val isSearching = uiState.query.isNotBlank()
+        viewModel.hideApp(app)
+        val messageRes =
+                if (isSearching) R.string.toast_excluded_from_results
+                else R.string.toast_excluded_from_suggestions
+        showUndoSnackbar(context.getString(messageRes, app.appName)) {
+            if (isSearching) {
+                viewModel.unhideAppFromResults(app)
+            } else {
+                viewModel.unhideAppFromSuggestions(app)
+            }
+        }
+    }
+
+    val onExcludeContactWithUndo: (ContactInfo) -> Unit = { contact ->
+        viewModel.excludeContact(contact)
+        showUndoSnackbar(
+                context.getString(R.string.toast_excluded_from_results, contact.displayName)
+        ) {
+            viewModel.removeExcludedContact(contact)
+        }
+    }
+
+    val onExcludeFileWithUndo: (DeviceFile) -> Unit = { file ->
+        viewModel.excludeFile(file)
+        showUndoSnackbar(
+                context.getString(R.string.toast_excluded_from_results, file.displayName)
+        ) {
+            viewModel.removeExcludedFile(file)
+        }
+    }
+
+    val onExcludeFileExtensionWithUndo: (DeviceFile) -> Unit = { file ->
+        val extension = FileUtils.getFileExtension(file.displayName)
+        if (extension != null) {
+            viewModel.excludeFileExtension(file)
+            val extensionLabel = ".${extension} files"
+            showUndoSnackbar(
+                    context.getString(R.string.toast_excluded_from_results, extensionLabel)
+            ) {
+                viewModel.removeExcludedFileExtension(extension)
+            }
+        }
+    }
+
+    val onExcludeSettingWithUndo: (DeviceSetting) -> Unit = { setting ->
+        viewModel.excludeSetting(setting)
+        showUndoSnackbar(
+                context.getString(R.string.toast_excluded_from_results, setting.title)
+        ) {
+            viewModel.removeExcludedSetting(setting)
+        }
+    }
+
+    val onExcludeAppShortcutWithUndo: (StaticShortcut) -> Unit = { shortcut ->
+        viewModel.excludeAppShortcut(shortcut)
+        showUndoSnackbar(
+                context.getString(
+                        R.string.toast_excluded_from_results,
+                        shortcutDisplayName(shortcut)
+                )
+        ) {
+            viewModel.removeExcludedAppShortcut(shortcut)
+        }
+    }
 
     // Set up toast callback for ViewModel
     val showToast: (Int) -> Unit = { stringResId ->
@@ -128,87 +234,138 @@ fun SearchRoute(
         }
     }
 
-    SearchScreen(
-            modifier = modifier,
-            state = uiState,
-            onQueryChanged = viewModel::onQueryChange,
-            onClearQuery = viewModel::clearQuery,
-            onRequestUsagePermission = viewModel::openUsageAccessSettings,
-            onSettingsClick = onSettingsClick,
-            onAppClick = viewModel::launchApp,
-            onAppInfoClick = viewModel::openAppInfo,
-            onUninstallClick = viewModel::requestUninstall,
-            onHideApp = viewModel::hideApp,
-            onPinApp = viewModel::pinApp,
-            onUnpinApp = viewModel::unpinApp,
-            onContactClick = viewModel::openContact,
-            onShowContactMethods = showContactMethodsBottomSheet,
-            onDismissContactMethods = dismissContactMethodsBottomSheet,
-            onCallContact = callContactWithPermission,
-            onSmsContact = viewModel::smsContact,
-            onContactMethodClick = viewModel::handleContactMethod,
-            onFileClick = viewModel::openFile,
-            onPinContact = viewModel::pinContact,
-            onUnpinContact = viewModel::unpinContact,
-            onExcludeContact = viewModel::excludeContact,
-            onPinFile = viewModel::pinFile,
-            onUnpinFile = viewModel::unpinFile,
-            onExcludeFile = viewModel::excludeFile,
-            onExcludeFileExtension = viewModel::excludeFileExtension,
-            onSettingClick = viewModel::openSetting,
-            onPinSetting = viewModel::pinSetting,
-            onUnpinSetting = viewModel::unpinSetting,
-            onExcludeSetting = viewModel::excludeSetting,
-            onAppShortcutClick = viewModel::launchAppShortcut,
-            onPinAppShortcut = viewModel::pinAppShortcut,
-            onUnpinAppShortcut = viewModel::unpinAppShortcut,
-            onExcludeAppShortcut = viewModel::excludeAppShortcut,
-            onIncludeAppShortcut = viewModel::removeExcludedAppShortcut,
-            onAppShortcutAppInfoClick = { shortcut -> viewModel.openAppInfo(shortcut.packageName) },
-            onPhoneNumberSelected = viewModel::onPhoneNumberSelected,
-            onDismissPhoneNumberSelection = viewModel::dismissPhoneNumberSelection,
-            onSearchTargetClick = { query, target -> viewModel.openSearchTarget(query, target) },
-            onSearchEngineLongPress = onSearchEngineLongPress,
-            onDirectSearchEmailClick = viewModel::openEmail,
-            onOpenAppSettings = viewModel::openAppSettings,
-            onOpenStorageAccessSettings = viewModel::openAllFilesAccessSettings,
-            onAppNicknameClick = { app ->
-                // This will be handled by the dialog state in SearchScreen
-            },
-            onClearDetectedShortcut = viewModel::clearDetectedShortcut,
-            onContactNicknameClick = { contact ->
-                // This will be handled by the dialog state in SearchScreen
-            },
-            onFileNicknameClick = { file ->
-                // This will be handled by the dialog state in SearchScreen
-            },
-            getAppNickname = getAppNickname,
-            getContactNickname = getContactNickname,
-            getFileNickname = getFileNickname,
-            onSaveAppNickname = viewModel::setAppNickname,
-            onSaveContactNickname = viewModel::setContactNickname,
-            onSaveFileNickname = viewModel::setFileNickname,
-            getSettingNickname = getSettingNickname,
-            onSaveSettingNickname = viewModel::setSettingNickname,
-            getLastShownPhoneNumber = viewModel::getLastShownPhoneNumber,
-            setLastShownPhoneNumber = viewModel::setLastShownPhoneNumber,
-            onDirectDialChoiceSelected = viewModel::onDirectDialChoiceSelected,
-            onDismissDirectDialChoice = viewModel::dismissDirectDialChoice,
-            onReleaseNotesAcknowledged = viewModel::acknowledgeReleaseNotes,
-            onWebSuggestionClick = { suggestion: String ->
-                viewModel.onWebSuggestionTap(suggestion)
-            },
-            onSearchEngineOnboardingDismissed = viewModel::onSearchEngineOnboardingDismissed,
-            onContactActionHintDismissed = viewModel::onContactActionHintDismissed,
-            onCustomizeSearchEnginesClick = onCustomizeSearchEnginesClick,
-            onDeleteRecentQuery = viewModel::deleteRecentQuery,
-            onWelcomeAnimationCompleted = onWelcomeAnimationCompleted,
-            onCustomAction = viewModel::onCustomAction,
-            getPrimaryContactCardAction = viewModel::getPrimaryContactCardAction,
-            getSecondaryContactCardAction = viewModel::getSecondaryContactCardAction,
-            onSavePrimaryContactCardAction = viewModel::setPrimaryContactCardAction,
-            onSaveSecondaryContactCardAction = viewModel::setSecondaryContactCardAction
-    )
+    Box(modifier = modifier.fillMaxSize()) {
+        SearchScreen(
+                modifier = Modifier.fillMaxSize(),
+                state = uiState,
+                onQueryChanged = viewModel::onQueryChange,
+                onClearQuery = viewModel::clearQuery,
+                onRequestUsagePermission = viewModel::openUsageAccessSettings,
+                onSettingsClick = onSettingsClick,
+                onAppClick = viewModel::launchApp,
+                onAppInfoClick = viewModel::openAppInfo,
+                onUninstallClick = viewModel::requestUninstall,
+                onHideApp = onHideAppWithUndo,
+                onPinApp = viewModel::pinApp,
+                onUnpinApp = viewModel::unpinApp,
+                onContactClick = viewModel::openContact,
+                onShowContactMethods = showContactMethodsBottomSheet,
+                onDismissContactMethods = dismissContactMethodsBottomSheet,
+                onCallContact = callContactWithPermission,
+                onSmsContact = viewModel::smsContact,
+                onContactMethodClick = viewModel::handleContactMethod,
+                onFileClick = viewModel::openFile,
+                onPinContact = viewModel::pinContact,
+                onUnpinContact = viewModel::unpinContact,
+                onExcludeContact = onExcludeContactWithUndo,
+                onPinFile = viewModel::pinFile,
+                onUnpinFile = viewModel::unpinFile,
+                onExcludeFile = onExcludeFileWithUndo,
+                onExcludeFileExtension = onExcludeFileExtensionWithUndo,
+                onSettingClick = viewModel::openSetting,
+                onPinSetting = viewModel::pinSetting,
+                onUnpinSetting = viewModel::unpinSetting,
+                onExcludeSetting = onExcludeSettingWithUndo,
+                onAppShortcutClick = viewModel::launchAppShortcut,
+                onPinAppShortcut = viewModel::pinAppShortcut,
+                onUnpinAppShortcut = viewModel::unpinAppShortcut,
+                onExcludeAppShortcut = onExcludeAppShortcutWithUndo,
+                onIncludeAppShortcut = viewModel::removeExcludedAppShortcut,
+                onAppShortcutAppInfoClick = { shortcut -> viewModel.openAppInfo(shortcut.packageName) },
+                onPhoneNumberSelected = viewModel::onPhoneNumberSelected,
+                onDismissPhoneNumberSelection = viewModel::dismissPhoneNumberSelection,
+                onSearchTargetClick = { query, target -> viewModel.openSearchTarget(query, target) },
+                onSearchEngineLongPress = onSearchEngineLongPress,
+                onDirectSearchEmailClick = viewModel::openEmail,
+                onOpenAppSettings = viewModel::openAppSettings,
+                onOpenStorageAccessSettings = viewModel::openAllFilesAccessSettings,
+                onAppNicknameClick = { app ->
+                    // This will be handled by the dialog state in SearchScreen
+                },
+                onClearDetectedShortcut = viewModel::clearDetectedShortcut,
+                onContactNicknameClick = { contact ->
+                    // This will be handled by the dialog state in SearchScreen
+                },
+                onFileNicknameClick = { file ->
+                    // This will be handled by the dialog state in SearchScreen
+                },
+                getAppNickname = getAppNickname,
+                getContactNickname = getContactNickname,
+                getFileNickname = getFileNickname,
+                onSaveAppNickname = viewModel::setAppNickname,
+                onSaveContactNickname = viewModel::setContactNickname,
+                onSaveFileNickname = viewModel::setFileNickname,
+                getSettingNickname = getSettingNickname,
+                onSaveSettingNickname = viewModel::setSettingNickname,
+                getLastShownPhoneNumber = viewModel::getLastShownPhoneNumber,
+                setLastShownPhoneNumber = viewModel::setLastShownPhoneNumber,
+                onDirectDialChoiceSelected = viewModel::onDirectDialChoiceSelected,
+                onDismissDirectDialChoice = viewModel::dismissDirectDialChoice,
+                onReleaseNotesAcknowledged = viewModel::acknowledgeReleaseNotes,
+                onWebSuggestionClick = { suggestion: String ->
+                    viewModel.onWebSuggestionTap(suggestion)
+                },
+                onSearchEngineOnboardingDismissed = viewModel::onSearchEngineOnboardingDismissed,
+                onContactActionHintDismissed = viewModel::onContactActionHintDismissed,
+                onCustomizeSearchEnginesClick = onCustomizeSearchEnginesClick,
+                onDeleteRecentQuery = viewModel::deleteRecentQuery,
+                onWelcomeAnimationCompleted = onWelcomeAnimationCompleted,
+                onCustomAction = viewModel::onCustomAction,
+                getPrimaryContactCardAction = viewModel::getPrimaryContactCardAction,
+                getSecondaryContactCardAction = viewModel::getSecondaryContactCardAction,
+                onSavePrimaryContactCardAction = viewModel::setPrimaryContactCardAction,
+                onSaveSecondaryContactCardAction = viewModel::setSecondaryContactCardAction
+        )
+
+        SnackbarHost(
+                hostState = snackbarHostState,
+                snackbar = { data ->
+                    val message = data.visuals.message
+                    val marker = " excluded from "
+                    val markerIndex = message.indexOf(marker)
+                    val annotatedMessage =
+                            if (markerIndex > 0) {
+                                buildAnnotatedString {
+                                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                                        append(message.substring(0, markerIndex))
+                                    }
+                                    append(message.substring(markerIndex))
+                                }
+                            } else {
+                                AnnotatedString(message)
+                            }
+                    val actionLabel = data.visuals.actionLabel
+                    Snackbar(
+                            action =
+                                    actionLabel?.let { label ->
+                                        {
+                                            TextButton(onClick = { data.performAction() }) {
+                                                Text(text = label)
+                                            }
+                                        }
+                                    },
+                            containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+                            contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                            actionContentColor = MaterialTheme.colorScheme.primary,
+                            shape = DesignTokens.ShapeLarge
+                    ) {
+                        Text(
+                                text = annotatedMessage,
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                        )
+                    }
+                },
+                modifier =
+                        Modifier.align(Alignment.BottomCenter)
+                                .imePadding()
+                                .padding(
+                                        start = DesignTokens.SpacingLarge,
+                                        end = DesignTokens.SpacingLarge,
+                                        bottom = DesignTokens.SpacingHuge
+                                )
+        )
+    }
 }
 
 data class ContactActionPickerDialogState(val contact: ContactInfo, val isPrimary: Boolean)
