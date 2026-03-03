3374815bb06ccd722fa3ad9ff183510ee2daea1d
Teja Karlapudi <teja2495@gmail.com>
2026-03-03 09:55:44 -0500

    fix file names

--- app/src/main/java/com/tk/quicksearch/search/searchScreen/SearchScreenLayout/SearchScreenLayoutConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.tk.quicksearch.search.searchScreen.searchScreenLayout
-
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.unit.dp
-
-/** Custom shape that rounds only the top corners */
-val TopRoundedShape =
-    RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp, bottomStart = 0.dp, bottomEnd = 0.dp)
